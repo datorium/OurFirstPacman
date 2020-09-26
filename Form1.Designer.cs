29725a7010e1efeae4e36f123fc05367e12dd0a6
Datorium <hello@datorium.lv>
2020-09-26 19:20:27 +0300

    version 19:20

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,12 +38,37 @@
             this.timerHero = new System.Windows.Forms.Timer(this.components);
             this.ScoreLabel = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pictureBox13 = new System.Windows.Forms.PictureBox();
+            this.pictureBox14 = new System.Windows.Forms.PictureBox();
+            this.timerEnemy = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.Hero)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Enemy)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Food)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).BeginInit();
             this.SuspendLayout();
             // 
             // Hero
@@ -51,7 +76,7 @@
             this.Hero.BackColor = System.Drawing.Color.Yellow;
             this.Hero.Location = new System.Drawing.Point(113, 68);
             this.Hero.Name = "Hero";
-            this.Hero.Size = new System.Drawing.Size(80, 80);
+            this.Hero.Size = new System.Drawing.Size(40, 40);
             this.Hero.TabIndex = 1;
             this.Hero.TabStop = false;
             this.Hero.Visible = false;
@@ -61,7 +86,7 @@
             this.Enemy.BackColor = System.Drawing.Color.CornflowerBlue;
             this.Enemy.Location = new System.Drawing.Point(238, 56);
             this.Enemy.Name = "Enemy";
-            this.Enemy.Size = new System.Drawing.Size(81, 80);
+            this.Enemy.Size = new System.Drawing.Size(40, 40);
             this.Enemy.TabIndex = 2;
             this.Enemy.TabStop = false;
             this.Enemy.Visible = false;
@@ -71,7 +96,7 @@
             this.Food.BackColor = System.Drawing.Color.Lime;
             this.Food.Location = new System.Drawing.Point(403, 56);
             this.Food.Name = "Food";
-            this.Food.Size = new System.Drawing.Size(81, 80);
+            this.Food.Size = new System.Drawing.Size(40, 40);
             this.Food.TabIndex = 3;
             this.Food.TabStop = false;
             this.Food.Visible = false;
@@ -111,29 +136,180 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.DarkSlateGray;
-            this.pictureBox1.Location = new System.Drawing.Point(34, 357);
+            this.pictureBox1.Location = new System.Drawing.Point(-3, -3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(410, 80);
+            this.pictureBox1.Size = new System.Drawing.Size(76, 80);
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "wall";
             this.pictureBox1.Visible = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.pictureBox3.Location = new System.Drawing.Point(-3, 68);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(76, 80);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "wall";
+            this.pictureBox3.Visible = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.pictureBox4.Location = new System.Drawing.Point(-3, 143);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(76, 80);
+            this.pictureBox4.TabIndex = 9;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "wall";
+            this.pictureBox4.Visible = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.pictureBox5.Location = new System.Drawing.Point(-3, 304);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(76, 80);
+            this.pictureBox5.TabIndex = 10;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Tag = "wall";
+            this.pictureBox5.Visible = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.pictureBox6.Location = new System.Drawing.Point(-3, 377);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(76, 80);
+            this.pictureBox6.TabIndex = 11;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Tag = "wall";
+            this.pictureBox6.Visible = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.pictureBox7.Location = new System.Drawing.Point(-3, 447);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(76, 80);
+            this.pictureBox7.TabIndex = 12;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Tag = "wall";
+            this.pictureBox7.Visible = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.pictureBox8.Location = new System.Drawing.Point(220, 357);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(76, 80);
+            this.pictureBox8.TabIndex = 13;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Tag = "wall";
+            this.pictureBox8.Visible = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.pictureBox9.Location = new System.Drawing.Point(-3, 229);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(76, 80);
+            this.pictureBox9.TabIndex = 14;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Tag = "wall";
+            this.pictureBox9.Visible = false;
             // 
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.DarkSlateGray;
-            this.pictureBox2.Location = new System.Drawing.Point(542, 215);
+            this.pictureBox2.Location = new System.Drawing.Point(291, 357);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(80, 222);
-            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.Size = new System.Drawing.Size(76, 80);
+            this.pictureBox2.TabIndex = 15;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "wall";
             this.pictureBox2.Visible = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.pictureBox10.Location = new System.Drawing.Point(373, 357);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(76, 80);
+            this.pictureBox10.TabIndex = 16;
+            this.pictureBox10.TabStop = false;
+            this.pictureBox10.Tag = "wall";
+            this.pictureBox10.Visible = false;
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.pictureBox11.Location = new System.Drawing.Point(441, 357);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(76, 80);
+            this.pictureBox11.TabIndex = 17;
+            this.pictureBox11.TabStop = false;
+            this.pictureBox11.Tag = "wall";
+            this.pictureBox11.Visible = false;
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.pictureBox12.Location = new System.Drawing.Point(437, 281);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(76, 80);
+            this.pictureBox12.TabIndex = 18;
+            this.pictureBox12.TabStop = false;
+            this.pictureBox12.Tag = "wall";
+            this.pictureBox12.Visible = false;
+            // 
+            // pictureBox13
+            // 
+            this.pictureBox13.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.pictureBox13.Location = new System.Drawing.Point(441, 208);
+            this.pictureBox13.Name = "pictureBox13";
+            this.pictureBox13.Size = new System.Drawing.Size(76, 80);
+            this.pictureBox13.TabIndex = 19;
+            this.pictureBox13.TabStop = false;
+            this.pictureBox13.Tag = "wall";
+            this.pictureBox13.Visible = false;
+            // 
+            // pictureBox14
+            // 
+            this.pictureBox14.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.pictureBox14.Location = new System.Drawing.Point(441, 142);
+            this.pictureBox14.Name = "pictureBox14";
+            this.pictureBox14.Size = new System.Drawing.Size(76, 80);
+            this.pictureBox14.TabIndex = 20;
+            this.pictureBox14.TabStop = false;
+            this.pictureBox14.Tag = "wall";
+            this.pictureBox14.Visible = false;
+            // 
+            // timerEnemy
+            // 
+            this.timerEnemy.Interval = 30;
+            this.timerEnemy.Tick += new System.EventHandler(this.timerEnemy_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(649, 525);
+            this.Controls.Add(this.pictureBox14);
+            this.Controls.Add(this.pictureBox13);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox10);
             this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.ScoreLabel);
             this.Controls.Add(this.ButtonStart);
@@ -148,7 +324,19 @@
             ((System.ComponentModel.ISupportInitialize)(this.Enemy)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Food)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -163,7 +351,20 @@
         private System.Windows.Forms.Timer timerHero;
         private System.Windows.Forms.Label ScoreLabel;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.PictureBox pictureBox13;
+        private System.Windows.Forms.PictureBox pictureBox14;
+        private System.Windows.Forms.Timer timerEnemy;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,16 +13,19 @@ namespace OurFirstPacman
     public partial class Form1 : Form
     {
         private int score = 0;
-        private int step = 2;
-        private int horHeroVelocity = 0;
-        private int verHeroVelocity = 0;
+        private int heroStep = 2;
+        private int enemyStep = 1;
+        private int heroHorVelocity = 0;
+        private int heroVerVelocity = 0;
 
         private int enemyHorVelocity = 0;
         private int enemyVerVelocity = 0;
+        private string enemyDirection = "down";
 
         private string heroDirection = "right";
         private int heroImageCount = 1;
         private int foodImageCount = 1;
+        private int enemyImageCount = 1;
 
         private bool heroIsPredator = false;
 
@@ -31,8 +34,8 @@ namespace OurFirstPacman
         public Form1()
         {
             InitializeComponent();
-            timer1.Start();
             timerHero.Start();
+            timerEnemy.Start();
 
             this.BackColor = Color.Blue;
             Hero.BackColor = Color.Transparent;
@@ -41,43 +44,101 @@ namespace OurFirstPacman
             Food.BackColor = Color.Transparent;
             Food.SizeMode = PictureBoxSizeMode.StretchImage;
             Food.Image = (Image)Properties.Resources.ResourceManager.GetObject("food_1");
+
+            Enemy.BackColor = Color.Transparent;
+            Enemy.SizeMode = PictureBoxSizeMode.StretchImage;
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             if(e.KeyCode == Keys.A)
             {
-                horHeroVelocity = -step;
-                verHeroVelocity = 0;
+                heroHorVelocity = -heroStep;
+                heroVerVelocity = 0;
                 heroDirection = "left";
             }
             else if(e.KeyCode == Keys.D)
             {
-                horHeroVelocity = step;
-                verHeroVelocity = 0;
+                heroHorVelocity = heroStep;
+                heroVerVelocity = 0;
                 heroDirection = "right";
             }
             else if(e.KeyCode == Keys.W)
             {
-                horHeroVelocity = 0;
-                verHeroVelocity = -step;
+                heroHorVelocity = 0;
+                heroVerVelocity = -heroStep;
                 heroDirection = "up";
             }
             else if(e.KeyCode == Keys.S)
             {
-                horHeroVelocity = 0;
-                verHeroVelocity = step;
+                heroHorVelocity = 0;
+                heroVerVelocity = heroStep;
                 heroDirection = "down";
             }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            Hero.Left += horHeroVelocity;
-            Hero.Top += verHeroVelocity;
+            MoveHero();
+            MoveEnemy();
+            EnemyBorderCollision();
             HeroBorderCollision();
             HeroEnemyCollision();
             HeroFoodCollision();
+            HeroWallCollision();
+        }
+
+        private void MoveHero()
+        {
+            Hero.Left += heroHorVelocity;
+            Hero.Top += heroVerVelocity;
+        }
+
+        private void MoveEnemy()
+        {
+            Enemy.Left += enemyHorVelocity;
+            Enemy.Top += enemyVerVelocity;
+
+            if (enemyDirection == "right")
+            {
+                enemyHorVelocity = enemyStep;
+                enemyVerVelocity = 0;
+            }
+            else if (enemyDirection == "left")
+            {
+                enemyHorVelocity = -enemyStep;
+                enemyVerVelocity = 0;
+            }
+            else if (enemyDirection == "down")
+            {
+                enemyVerVelocity = enemyStep;
+                enemyHorVelocity = 0;
+            }
+            else if (enemyDirection == "up")
+            {
+                enemyVerVelocity = -enemyStep;
+                enemyHorVelocity = 0;
+            }
+        }
+
+        private void EnemyBorderCollision()
+        {
+            if (Enemy.Left + Enemy.Width < 0)
+            {
+                Enemy.Left = ClientRectangle.Width;
+            }
+            if (Enemy.Left > ClientRectangle.Width)
+            {
+                Enemy.Left = 0 - Enemy.Width;
+            }
+            if (Enemy.Top + Enemy.Height < 0)
+            {
+                Enemy.Top = ClientRectangle.Height;
+            }
+            if (Enemy.Top > ClientRectangle.Height)
+            {
+                Enemy.Top = 0 - Enemy.Height;
+            }
         }
 
         private void HeroFoodCollision()
@@ -156,15 +217,23 @@ namespace OurFirstPacman
             ScoreLabel.Text = "Score: " + score;
         }
 
-
         private void ButtonStart_Click(object sender, EventArgs e)
         {
             Hero.Visible = true;
             Enemy.Visible = true;
             Food.Visible = true;
 
+            foreach(Control c in Controls)
+            {
+                if((string)c.Tag == "wall")
+                {
+                    c.Visible = true;
+                }
+            }
+
             ButtonStart.Visible = false;
             this.Focus();
+            timer1.Start();
         }
 
         private void timerHero_Tick(object sender, EventArgs e)
@@ -176,6 +245,50 @@ namespace OurFirstPacman
             if(heroImageCount > 4)
             {
                 heroImageCount = 1;
+            }
+        }
+
+        private void HeroWallCollision()
+        {
+            foreach(Control c in Controls)
+            {
+                if((string)c.Tag == "wall")
+                {
+                    if (c.Bounds.IntersectsWith(Hero.Bounds))
+                    {
+                        heroHorVelocity = 0;
+                        heroVerVelocity = 0;
+                    }
+                }
+            }
+        }
+
+        private void timerEnemy_Tick(object sender, EventArgs e)
+        {
+            string imageName;
+            imageName = "enemy_" + enemyDirection + "_" + enemyImageCount;
+            Enemy.Image = (Image)Properties.Resources.ResourceManager.GetObject(imageName);
+            enemyImageCount += 1;
+            if (enemyImageCount > 2)
+            {
+                enemyImageCount = 1;
+            }
+
+            if (Hero.Left - Enemy.Left > Hero.Top - Enemy.Top && Hero.Left - Enemy.Left > 0)
+            {
+                enemyDirection = "right";
+            }
+            else if (Hero.Left - Enemy.Left < Hero.Top - Enemy.Top && Hero.Left - Enemy.Left < 0)
+            {
+                enemyDirection = "left";
+            }
+            else if (Hero.Top - Enemy.Top > 0)
+            {
+                enemyDirection = "down";
+            }
+            else if (Hero.Top - Enemy.Top < 0)
+            {
+                enemyDirection = "up";
             }
         }
     }
